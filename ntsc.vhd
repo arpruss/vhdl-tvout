06bcbf51dad93309581338e8487cbc60be608211
arpruss <arpruss@gmail.com>
2017-09-05 20:47:47 -0500

    640x480xmove

--- ntsc.vhd
+++ ntsc.vhd
@@ -34,19 +34,19 @@ use work.ntsclib.all;
 entity ntsc is
     generic
     (
-    clockFrequency : real := 160000000.0;
-    pwmBits : natural := 5
+    clockFrequency : real := 160.0e6;
+    pwmBits : natural := 4
     );    
 
     port
     (
     sync_output : out std_logic;
     bw_output : out std_logic;
-    clock: in std_logic
-    --x : out unsigned(9 downto 0);
-    --y : out unsigned(8 downto 0);
-    --req   : out std_logic; -- flips to request a pixel
-    --pixel : in unsigned(pwmBits-1 downto 0)
+    clock: in std_logic;
+    x : out unsigned(9 downto 0);
+    y : out unsigned(8 downto 0);
+    req   : out std_logic := '0'; -- set to request a pixel
+    pixel : in unsigned(pwmBits-1 downto 0)
     );
     function microsToClock(us : real) return natural is
     begin
@@ -55,37 +55,42 @@ entity ntsc is
 end ntsc;
 
 architecture behavioral of ntsc is
+    constant HALF_LINE : natural := microsToClock(63.6/2.0);
+    constant halfLineBits : natural := natural(ceil(log2(real(HALF_LINE))));
+
     constant pwmLevels : natural := 2**pwmBits;
-    signal frameCount : unsigned(12 downto 0) := to_unsigned(0,13);
     signal halfLine : unsigned(9 downto 0) := to_unsigned(0,10);
-    signal horizHCount : unsigned(7+pwmBits downto 0) := to_unsigned(0,8+pwmBits); -- clock count within halfline
+    signal horizHCount : unsigned(halfLineBits-1 downto 0) := to_unsigned(0,halfLineBits); -- clock count within halfline
     signal field : std_logic := '1';
     
-    constant DATA_HORIZ_START : natural := (microsToClock(6.2+4.7)+pwmLevels-1)/pwmLevels*pwmLevels;
-    constant DATA_HORIZ_END : natural := microsToClock(63.5-1.5)/pwmLevels*pwmLevels;
+    constant DATA_LENGTH : natural := 640*pwmLevels;
+    constant DATA_HORIZ_START : natural := (microsToClock((63.6-1.5+6.2+4.7)/2)-DATA_LENGTH/2)/pwmLevels*pwmLevels;
+    constant DATA_HORIZ_END : natural := DATA_HORIZ_START + DATA_LENGTH;
+    
+--    constant DATA_HORIZ_START : natural := (microsToClock(6.2+4.7)+pwmLevels-1)/pwmLevels*pwmLevels + 15*pwmLevels;
+--    constant DATA_HORIZ_END : natural := microsToClock(63.6-1.5)/pwmLevels*pwmLevels - 21*pwmLevels;
     
 begin 
     -- ************************************************************************
     --                          NTSC OUT
     -- ************************************************************************
+    --req <= '0';
 
-    
     process (clock)
     variable ntscEq, ntscSe: std_logic;
     variable displayLine : unsigned(9 downto 0);
-    variable horizCount : unsigned(8+pwmBits downto 0);
+    variable horizCount : unsigned(halfLineBits downto 0);
     variable horizCountAdj : unsigned(horizCount'length-1 downto 0);
-    variable dataLine : boolean;
+    variable dataRegion : boolean;
+    
+    
     begin
         if rising_edge(clock) then
-            if horizHCount = microsToClock(63.5/2.0)-1 then
+            if horizHCount = HALF_LINE-1 then
                 horizHCount <= to_unsigned(0,horizHCount'length);
                 if halfLine = 524 then
                     field <= not field; 
                     halfLine <= to_unsigned(0,halfLine'length);
-                    if field = '0' then
-                        frameCount <= frameCount + 1;
-                    end if;
                 else
                     halfLine <= halfLine + 1;
                 end if;
@@ -94,45 +99,60 @@ begin
             end if;
             
             if field = '1' and 18 <= halfLine then
-                displayLine := resize(halfLine - 18, displayLine'length)(9 downto 1) & '0';
-                dataLine := true;
+                displayLine := resize(halfLine - 40, displayLine'length)(9 downto 1) & '0';
+                dataRegion := true;
                 if halfLine(0) = '0' then
                     horizCount := resize(horizHCount, horizCount'length);
                 else
-                    horizCount := resize(horizHCount, horizCount'length) + microsToClock(63.5/2.0);
+                    horizCount := resize(horizHCount, horizCount'length) + HALF_LINE;
                 end if;
             elsif field = '0' and 19 <= halfLine then
-                displayLine := resize(halfLine - 19, displayLine'length)(9 downto 1) & '1';
-                dataLine := true;
+                displayLine := resize(halfLine - 41, displayLine'length)(9 downto 1) & '1';
+                dataRegion := true;
                 if halfLine(0) = '1' then
                     horizCount := resize(horizHCount, horizCount'length);
                 else
-                    horizCount := resize(horizHCount, horizCount'length) + microsToClock(63.5/2.0);
+                    horizCount := resize(horizHCount, horizCount'length) + HALF_LINE;
                 end if;
             else
-                dataLine := false;
+                dataRegion := false;
                 displayLine := to_unsigned(0, displayLine'length);
                 horizCount := to_unsigned(0, horizCount'length);
             end if;
 
-            if dataLine then
-                if horizCount >= DATA_HORIZ_END then
+            if dataRegion then
+                if horizCount >= DATA_HORIZ_END-pwmLevels then
                     sync_output <= '1';
                     bw_output <= '0';
                 elsif horizCount < microsToClock(4.7) then 
                     sync_output <= '0';
                     bw_output <= '0';
+                elsif halfLine < 40 or halfLine >= 40+480 then
+                    sync_output <= '1';
+                    bw_output <= '0';
+                elsif horizCount = DATA_HORIZ_START-pwmLevels then
+                    x <= to_unsigned(0, x'length);
+                    y <= resize(displayLine, y'length);
+                elsif horizCount = DATA_HORIZ_START-pwmLevels/2 then
+                    req <= '0';
+                elsif horizCount = DATA_HORIZ_START-pwmLevels+1 then
+                    req <= '1';
                 elsif horizCount < DATA_HORIZ_START then
                     sync_output <= '1';
-                    bw_output <= '0';
-                elsif halfLine < 40 then
-                    sync_output <= '1';
-                    bw_output <= '0';
+                    bw_output <= '0';                
                 else
                     sync_output <= '1';
+                    if horizCount(pwmBits-1 downto 0) = pwmLevels-1 then                        
+                        x <= resize(horizCount(horizCount'length-1 downto pwmBits)-(DATA_HORIZ_START / pwmLevels), x'length);
+                        y <= resize(displayLine, y'length);
+                    elsif horizCount(pwmBits-1 downto 0) = 0 and horizCount < DATA_HORIZ_END - pwmLevels then
+                        req <= '1';
+                    elsif horizCount(pwmBits-1 downto 0) = pwmLevels/2 then
+                        req <= '0';
+                    end if;
                     -- active part of screen
-                    horizCountAdj := horizCount + (resize(displayLine,12) sll (pwmBits-1)) + (frameCount sll 1);
-                    if horizCountAdj((6+pwmBits) downto 7) >= horizCountAdj((pwmBits-1) downto 0) then
+                    --horizCountAdj := horizCount + (resize(displayLine,horizCount'length) sll (pwmBits-1)) + resize(frameCount sll 1,horizCount'length);
+                    if pixel >= horizCount(pwmBits-1 downto 0) then
                         bw_output <= '1';
                     else
                         bw_output <= '0';
--- tvout.vhd
+++ tvout.vhd
@@ -14,13 +14,53 @@ entity tvout is
 end tvout;    
 
 architecture behavioral of tvout is
+    constant pwmBits : natural := 4;
+    constant clockFrequency : real := 212.5e6; --212500000.0;
     signal clock : std_logic; 
-    signal alpha : std_logic;
-    constant clockFrequency : real := 160000000.0;
+    signal req: std_logic;
+    signal x : unsigned(9 downto 0);
+    signal y : unsigned(8 downto 0);
+    signal pixel: unsigned(pwmBits-1 downto 0) ;
+    signal posX : signed(10 downto 0) := to_signed(320,11);
+    signal vX : signed(1 downto 0) := to_signed(1,2);
+    signal posY : signed(9 downto 0) := to_signed(240,10);
+    signal vY : signed(1 downto 0) := to_signed(1,2);
 begin
     PLL_INSTANCE: entity work.pll port map(main_clock, clock);
     output: entity work.ntsc 
-                generic map(clockFrequency => clockFrequency) 
-                port map(sync_output=>sync_output, bw_output=>bw_output, clock=>clock);
+                generic map(clockFrequency => clockFrequency, pwmBits=>pwmBits) 
+                port map(sync_output=>sync_output, bw_output=>bw_output, clock=>clock, pixel=>pixel, req=>req, x=>x, y=>y);
+
+    process(req)
+    variable xs : signed(10 downto 0);
+    variable ys : signed(9 downto 0);
+    variable dist2 : signed(xs'length+ys'length downto 0);
+    variable distScaled : unsigned(xs'length+ys'length-11 downto 0);
+    begin
+        if rising_edge(req) then
+            if x = 0 and y = 0 then
+                if posX+vX > 639 then
+                    vX <= to_signed(-1, vX'length);
+                elsif posX+vX < 0 then
+                    vX <= to_signed(1, vX'length);
+                end if;
+                if posY+vY > 479 then
+                    vY <= to_signed(-1, vY'length);
+                elsif posY+vY < 0 then
+                    vY <= to_signed(1, vY'length);
+                end if;
+                posX <= posX + vX;
+                posY <= posY + vY;
+            end if;
+            xs := signed(resize(x,11))-posX;
+            ys := signed(resize(y,10))-posY;
+            distScaled := resize(unsigned(xs*xs+ys*ys) srl 11,distScaled'length);
+            if distScaled < 2**pwmBits then
+                pixel <= 2**pwmBits-1-resize(distScaled,pwmBits);
+            else
+                pixel <= to_unsigned(0,pwmBits);
+            end if;
+        end if;
+    end process;
 end behavioral;
 
