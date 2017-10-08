4ad7276d4cd897f2f5af2815e06f677ff37a5249
arpruss <arpruss@gmail.com>
2017-10-08 09:05:26 -0500

    new frame marker

--- tvout.vhd
+++ tvout.vhd
@@ -35,26 +35,20 @@ architecture behavioral of tvout is
     signal posY : signed(9 downto 0) := to_signed(240,10);
     signal vY : signed(1 downto 0) := to_signed(1,2);
     signal ch : integer range 0 to 127;
-    
+    signal new_frame : std_logic;
 
 begin
     PLL_INSTANCE: entity work.pll port map(main_clock, clock);
     output: entity work.ntsc 
                 generic map(clockFrequency => clockFrequency, pwmBits=>pwmBits, screenWidth=>screenWidth) 
                 port map(sync_output=>sync_output, bw_output=>bw_output, clock=>clock, pixel=>pixel, 
-                    req=>req, x=>x, y=>y, new_frame=>open);
+                    req=>req, x=>x, y=>y, new_frame=>new_frame);
 
-    process(req)
-
-    variable xs : signed(10 downto 0);
-    variable ys : signed(9 downto 0);
-    variable dist2 : signed(xs'length+ys'length downto 0);
-    variable distScaled : unsigned(xs'length+ys'length-11 downto 0);
-    begin
-        if rising_edge(req) then
-            if x=screenWidth-1 and y=479 then
-                --note that (screenWidth-1,479) will be invisible on a typical TV, so if we miss
-                --the timing on it, no harm done; we basically have all the vertical blanking
+    process(clock,new_frame)
+    begin 
+        if rising_edge(clock) then
+            if new_frame = '1' then
+                --we  have all the vertical blanking
                 --time available for setting up the next frame.
                 if posX >= screenWidth-1 then
                     vX <= to_signed(-1, vX'length);
@@ -80,20 +74,30 @@ begin
 --                else
 --                    pixel <= to_unsigned(0, pixel'length);
 --                end if;
+            end if;
+        end if;
+    end process;
+                    
+    process(req)
+
+    variable xs : signed(10 downto 0);
+    variable ys : signed(9 downto 0);
+    variable dist2 : signed(xs'length+ys'length downto 0);
+    variable distScaled : unsigned(xs'length+ys'length-11 downto 0);
+    begin
+        if rising_edge(req) then
+            xs := signed(resize(x,11))-posX;
+            ys := signed(resize(y,10))-posY;
+            distScaled := resize(unsigned(xs*xs+ys*ys) srl 11,distScaled'length);
+            if button = '0' and (
+                resize(signed(x)-integer(screenWidth/2),10) = resize(signed(y)-integer(240),10) or 
+                resize(signed(x)-integer(screenWidth/2),10) = resize(integer(240)-signed(y),10) ) then
+                pixel <= to_unsigned(255,pwmBits);
             else
-                xs := signed(resize(x,11))-posX;
-                ys := signed(resize(y,10))-posY;
-                distScaled := resize(unsigned(xs*xs+ys*ys) srl 11,distScaled'length);
-                if button = '0' and (
-                    resize(signed(x)-integer(screenWidth/2),10) = resize(signed(y)-integer(240),10) or 
-                    resize(signed(x)-integer(screenWidth/2),10) = resize(integer(240)-signed(y),10) ) then
-                    pixel <= to_unsigned(255,pwmBits);
+                if distScaled < 2**pwmBits then
+                    pixel <= 2**pwmBits-1-resize(distScaled,pwmBits);
                 else
-                    if distScaled < 2**pwmBits then
-                        pixel <= 2**pwmBits-1-resize(distScaled,pwmBits);
-                    else
-                        pixel <= to_unsigned(0,pwmBits);
-                    end if;
+                    pixel <= to_unsigned(0,pwmBits);
                 end if;
             end if;
          end if;
